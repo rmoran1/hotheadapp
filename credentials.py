56aec7792fdd63dfe2b51f4fade594d5e7ff88a4
Ralph Moran <rmoran1@nd.edu>
2018-04-27 02:02:00 -0400

    Ignore

--- credentials.py
+++
@@ -1,9 +0,0 @@
-# Twitter App access keys for @user
-
-# Consume:
-CONSUMER_KEY    = 'X2KtBAjyCRAILdp58Y015UNOS'
-CONSUMER_SECRET = 'RUilmpjccK24ewD3jCCBw6U5FghEmOZdtW8wLOyzZBZUFmlUgz'
-
-# Access:
-ACCESS_TOKEN  = '216408277-wAcNwLvpnrexKAC0DuFWroQ0Q49L4sqH71e2jMxL'
-ACCESS_SECRET = 'pCneOmEnKnpzZrBsofzR6GRyeHmXogTFMiFROjqBDYQmT'
